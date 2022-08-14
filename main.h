9796b92947a33d57ba18511111388a5623b2c66b
Eburnfelix93 <eburnfelixx@gmail.com>
2022-08-14 11:44:39 +0300

    printf

--- main.h
+++ main.h
@@ -1,31 +1,33 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-/**
- * struct specifiers - Struct specifiers
- * @specifier: The conversion specifier
- * @f: The function pointer
- */
-struct convert
-{
-	char *sym;
-	int (*f)(va_list);
-};
-typedef struct convert conver_t;
 
-int _write(char c);
+
+/**
+ * struct format - Struct for format
+ * @specifiers: Struct format
+ * @f: The function associated
+ */
+
+typedef struct specifiers
+{
+	char specifiers;
+	int (*f)(va_list);
+} specifiers_t;
+
+/*prototypes*/
 int _printf(const char *format, ...);
-int _print_a_char(va_list args);
-int _print_a_string(va_list args);
-int _print_format(const char *format, va_list args);
-int _print_spec(char format, va_list args);
-int _print_invalid_spec(char prev_format, char format, int count);
-int _print_a_integer(va_list args);
-void _recursion_integer(int a);
-int _print_int_binary(va_list args);
-void _recursion_int_binary(int a);
-int _validate_char(char _type);
-#endif 
+int get_function(char s, va_list args);
+int _putchar(char c);
+
+/*Conversion specifiers*/
+int print_char(va_list args);
+int print_string(va_list args);
+int print_digit(va_list args);
+int print_mod(va_list args);
+int print_rev_string(va_list args);
+
+#endif
